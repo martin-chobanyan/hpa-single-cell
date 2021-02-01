b405bac003fe57c57994ada48074c72346c09a38
Martin Chobanyan <chobanyan.martin@gmail.com>
2021-01-31 23:06:10 -0500

    added RLE encoding function

---
+++ hpa/utils/__init__.py
@@ -0,0 +1,25 @@
+import base64
+import numpy as np
+from pycocotools import _mask as coco_mask
+import zlib
+
+
+def encode_binary_mask(mask):
+    if mask.dtype != np.bool:
+        raise ValueError(f"encode_binary_mask expects a binary mask, received dtype == {mask.dtype}")
+    mask = np.squeeze(mask)
+    if len(mask.shape) != 2:
+        raise ValueError(f"encode_binary_mask expects a 2d mask, received shape == {mask.shape}")
+
+    # convert input mask to expected COCO API input --
+    mask_to_encode = mask.reshape(mask.shape[0], mask.shape[1], 1)
+    mask_to_encode = mask_to_encode.astype(np.uint8)
+    mask_to_encode = np.asfortranarray(mask_to_encode)
+
+    # RLE encode mask --
+    encoded_mask = coco_mask.encode(mask_to_encode)[0]["counts"]
+
+    # compress and base64 encoding --
+    binary_str = zlib.compress(encoded_mask, zlib.Z_BEST_COMPRESSION)
+    base64_str = base64.b64encode(binary_str)
+    return base64_str
