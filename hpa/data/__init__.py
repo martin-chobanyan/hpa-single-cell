6aa81a7f8f7f00b35cf2c2e6a9d3e9da54b33011
Martin Chobanyan <chobanyan.martin@gmail.com>
2021-02-14 12:42:19 -0500

    added data -> misc imodule

--- hpa/data/__init__.py
+++ hpa/data/__init__.py
@@ -1,5 +1,1 @@
 from .dataset import *
-
-
-def parse_string_label(label_str):
-    return [int(i) for i in label_str.split('|')]
---
+++ hpa/data/misc.py
@@ -0,0 +1,2 @@
+def parse_string_label(label_str):
+    return [int(i) for i in label_str.split('|')]
