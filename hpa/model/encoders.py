145d6c69356bccc4846734ca6d52205377471601
Martin Chobanyan <chobanyan.martin@gmail.com>
2021-02-19 01:24:20 -0500

    added code for loading pretrained "bestfitting" winning models from previous competitiont

--- hpa/model/encoders.py
+++ hpa/model/encoders.py
@@ -13,3 +13,16 @@ class DPNEncoder(Module):
             if isinstance(x, tuple):
                 x = torch.cat(x, dim=1)
         return x
+
+
+class GreenAndReferenceEncoder(Module):
+    def __init__(self, ref_encoder, n_green_hidden_channels, n_output_channels):
+        super().__init__()
+        self.n_green_hidden_channels = n_green_hidden_channels
+        self.n_output_channels = n_output_channels
+
+        self.green_encoder = None
+        self.ref_encoder = ref_encoder
+
+    def forward(self, x):
+        return x
