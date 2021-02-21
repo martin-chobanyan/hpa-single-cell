ab1803b6bf59bff4e027513f8dc645e9430bd875
Martin Chobanyan <chobanyan.martin@gmail.com>
2021-02-21 16:29:21 -0500

    added transformation to transform cell segmentation to binary segmentations

--- hpa/data/transforms.py
+++ hpa/data/transforms.py
@@ -1,6 +1,29 @@
 import albumentations as A
+import numpy as np
 
 
 class HPACompose(A.Compose):
     def __init__(self, transforms, *args, **kwargs):
         super().__init__(transforms, additional_targets={'ref': 'image'}, *args, **kwargs)
+
+
+class ToBinaryCellSegmentation(A.ImageOnlyTransform):
+    def __init__(self, dtype=np.float32):
+        super().__init__(p=1.0)
+        self.dtype = dtype
+
+    def apply(self, img, **params):
+        # 1 if cell is present, 0 otherwise
+        img = ~(img == 0)
+        img = img.astype(self.dtype)
+        return img
+
+    @property
+    def targets(self):
+        return {"image": self.apply}
+
+    def get_params_dependent_on_targets(self, params):
+        return ()
+
+    def get_transform_init_args_names(self):
+        return ()
