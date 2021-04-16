bd324b0bf06f7d4b22cf0ea040e25343f8158398
Martin Chobanyan <chobanyan.martin@gmail.com>
2021-04-15 22:46:52 -0400

    added LSE layer

--- hpa/model/layers.py
+++ hpa/model/layers.py
@@ -1,10 +1,11 @@
+import torch
 from torch.nn import BatchNorm2d, Conv2d, Module, ReLU
 
 
 class ConvBlock(Module):
     def __init__(self, in_channels, out_channels, kernel_size, bnorm=True, relu=True, bias=True):
         super().__init__()
-        self.conv = Conv2d(in_channels, out_channels, kernel_size, bias=bias, padding=int(kernel_size/2))
+        self.conv = Conv2d(in_channels, out_channels, kernel_size, bias=bias, padding=int(kernel_size / 2))
         self.bnorm = None
         if bnorm:
             self.bnorm = BatchNorm2d(out_channels)
@@ -19,3 +20,30 @@ class ConvBlock(Module):
         if self.relu is not None:
             x = self.relu(x)
         return x
+
+
+class LogSumExp(Module):
+    """Log-Sum-Exponential averaging layer for 2D matrices(interpolates between maximum and average)"""
+
+    def __init__(self, r=5, dim=0, keepdim=True):
+        """Initialization
+
+        Parameters
+        ----------
+        r: float, optional
+            The interpolation parameter (r = 1 ~ average, r >> 1 ~ maximum; default = 5)
+        dim: int, optional
+            The dimension to sum across (default = 0)
+        keepdim: bool, optional
+            Whether or not to keep the reduced dimension (default = True)
+        """
+        super().__init__()
+        self.r = r
+        self.dim = dim
+        self.keepdim = keepdim
+
+    def forward(self, x):
+        assert len(x.shape) == 2, 'Input must be a 2D matrix tensor'
+        n, _ = x.shape
+        lse = torch.logsumexp(self.r * x, dim=self.dim, keepdim=self.keepdim)
+        return (1 / self.r) * (lse + torch.log(torch.tensor(1 / n)))
